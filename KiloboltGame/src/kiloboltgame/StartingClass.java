3554a0940d062f58990eeb89a1c6a323aea613bf
sy7kenny <syfung@ucdavis.edu>
2015-02-08 23:30:28 -0800

    FIrst commit! Yay!

---
+++ KiloboltGame/src/kiloboltgame/StartingClass.java
@@ -0,0 +1,167 @@
+package kiloboltgame;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+	private Robot robot;
+	private Image image, currentSprite, character, characterDown,
+			characterJumped, background;
+	private Graphics second;
+	private URL base;
+	private static Background bg1, bg2;
+
+	@Override
+	public void init() {
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+
+		}
+		// Image Setups
+		character = getImage(base, "data/character.png");
+		characterDown = getImage(base, "data/down.png");
+		characterJumped = getImage(base, "data/jumped.png");
+		currentSprite = character;
+		background = getImage(base, "data/background.png");
+	}
+
+	@Override
+	public void start() {
+
+		bg1 = new Background(0, 0);
+		bg2 = new Background(2160, 0);
+		robot = new Robot();
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void stop() {
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			robot.update();
+			if (robot.isJumped()) {
+				currentSprite = characterJumped;
+			} else if (robot.isJumped() == false && robot.isDucked() == false) {
+				currentSprite = character;
+			}
+			bg1.update();
+			bg2.update();
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+	}
+
+	public void paint(Graphics g) {
+		g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
+		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
+		g.drawImage(currentSprite, robot.getCenterX() - 61,
+				robot.getCenterY() - 63, this);
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+		case KeyEvent.VK_DOWN:
+			currentSprite = characterDown;
+			if (robot.isJumped() == false) {
+				robot.setDucked(true);
+				robot.setSpeedX(0);
+			}
+			break;
+		case KeyEvent.VK_LEFT:
+			robot.moveLeft();
+			robot.setMovingLeft(true);
+			break;
+		case KeyEvent.VK_RIGHT:
+			robot.moveRight();
+			robot.setMovingRight(true);
+			break;
+		case KeyEvent.VK_SPACE:
+			robot.jump();
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+		case KeyEvent.VK_DOWN:
+			currentSprite = character;
+			robot.setDucked(false);
+			break;
+		case KeyEvent.VK_LEFT:
+			robot.stopLeft();
+			break;
+		case KeyEvent.VK_RIGHT:
+			robot.stopRight();
+			break;
+		case KeyEvent.VK_SPACE:
+			
+			break;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static Background getBg1() {
+		return bg1;
+	}
+
+	public static Background getBg2() {
+		return bg2;
+	}
+}
